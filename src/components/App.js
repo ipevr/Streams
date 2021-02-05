311cd37c05d58d84b7789f429e7520a61e1d1d85
Ingo Peters <ingo.peters2@gmx.de>
2021-02-05 12:14:13 +0100

    Example for BAD navigation
    Never use ankertags for navigation inside the app. This causes to load all of the data, scripts, external links and whatever is listed in index.html a second time - what we definitely don't want!

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <a href="/pagetwo">Navigate to page two</a>
+    </div>
+  );
 };
 
 const PageTwo = () => {
@@ -10,6 +15,7 @@ const PageTwo = () => {
     <div>
       PageTwo
       <button>Click Me</button>
+      <a href="/">Navigate to page one</a>
     </div>
   );
 };
