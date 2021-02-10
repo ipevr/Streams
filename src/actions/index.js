f21046ef524f0798ae69d50a39a35ca3ab7e5507
Ingo Peters <ingo.peters2@gmx.de>
2021-02-10 15:45:41 +0100

    Now we can reference to the router history...
    ...to navigate back to the home route after we have successfully created a new stream. The action creator can reference to the history now, so that we can programmatically link to the home route from the action creator.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,10 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  console.log(response);
-  // Do some programmatic navigation to get the user back to the root route
-  // if the response of creating the stream was successfull.
-  // response.statusText === "Created"
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
