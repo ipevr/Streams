a5b206a153de62625f781ac0eb23492e3dbaa57f
Ingo Peters <ingo.peters2@gmx.de>
2021-02-10 14:50:49 +0100

    Streams List shows a button to create new stream
    - Only displayed when the user is logged in.
    - Clicking the button navigates the user to the /create/new page.

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
@@ -7,18 +8,43 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (this.props.userId === stream.userId) {
+      return (
+        <div>
+          <button className="btn btn-primary m-1">Edit</button>
+          <button className="btn btn-danger ms-1">Delete</button>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  renderAdminCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div className="d-flex justify-content-end">
+          <Link to="/streams/new" className="btn btn-secondary">
+            Create new stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
-        <li key={stream.id} className="list-group-item d-flex">
+        <li key={stream.id} className="list-group-item d-flex pe-0">
           <i
-            className="bi bi-camera-reels-fill me-3"
+            className="bi bi-camera-reels-fill me-3 align-self-start"
             style={{ fontSize: "2rem" }}
           ></i>
-          <div>
+          <div className="align-self-start flex-fill">
             <h4>{stream.title}</h4>
             <p>{stream.description}</p>
           </div>
+          {this.renderAdmin(stream)}
         </li>
       );
     });
@@ -28,14 +54,23 @@ class StreamList extends React.Component {
     return (
       <div className="container-fluid">
         <h2>Streams:</h2>
-        <ul className="list-group list-group-flush">{this.renderList()}</ul>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item d-flex"></li>
+          {this.renderList()}
+          <li className="list-group-item d-flex"></li>
+        </ul>
+        {this.renderAdminCreate()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    userId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
